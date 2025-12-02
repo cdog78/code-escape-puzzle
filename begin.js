934815846a57951f660247830b775693c4ee4035
cdog78 <102875282+cdog78@users.noreply.github.com>
2025-12-02 22:19:23 +0000

    Update begin.js

--- begin.js
+++ begin.js
@@ -1,1 +1,5 @@
+const encryptedcontent = "U2FsdGVkX18R7KIStJaLEEYXzi5U/s2AbBZc2HTEXCfP3GoseafuuM9YSg9edZegec31FXAyuY4wvG7IcTMgMX1SPQ4uI2Tg/jbkL5tUJbQ="
 
+function includecontent(key){
+  document.getElementById('includedcontent').innerHTML = decrypt(encryptedcontent, key);
+}
